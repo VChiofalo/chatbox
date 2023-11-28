8243b2c36dab3d4b64296e15bd9bdde45c73f1d2
VChiofalo <vincent.chiofalo@gmail.com>
2023-11-28 12:51:02 +0100

    connection fait

--- index.js
+++ index.js
@@ -11,15 +11,24 @@ const serve =  serveStatic(path.join(__dirname, 'public'), {index: 'index.html'}
 const httpServer = http.createServer((req,res) =>
     serve(req, res, finalhandler(req, res))
 );
-httpServer.listen(9001, () => { console.log(`http://localhost:9001`); });
+httpServer.listen(9000, () => { console.log(`http://localhost:9000`); });
 
 //----------------------------------------------------------
 // Mise en place des WebSockets
 //----------------------------------------------------------
+let users = [];
 const io = new Server(httpServer);
 io.on('connection', (socket) => {
     console.log('nouveau !!!');
-    socket.on('message', (message) => {
-        console.log(message)
+
+    socket.on('client:user:pseudo', (user) => {
+        if (users.includes(user)) {
+            console.log('ici');
+            socket.emit('server:user:exist')
+        } else {
+            users.push(user)
+            socket.emit('server:user:connected')
+        }
+        console.table(users);
     })
 });
--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -1,6 +1,31 @@
 import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const socket = io.connect(document.location.host);
 
-const socket = io.connect(document.location.host);
-document.querySelector("#button_test").addEventListener('click', () => {
-    socket.emit('message',`Message test pour le serveur`);
-});
+    let btnCo = document.querySelector('#button_connexion');
+    let chat = document.querySelector('#chat')
+
+    function connectByPseudo(exists){
+        if (exists) {
+            window.alert('Pseudo déjà attribué à un autre utilisateur !')
+        }
+        let user = window.prompt('Choisissez un pseudo : ')
+        if (user !== null && user !== '') {
+            socket.emit('client:user:pseudo', user);
+        }
+    }
+
+    function connectToChat(){
+        chat.classList.toggle('invisible');
+        btnCo.classList.toggle('invisible');
+    }
+    
+    btnCo.addEventListener('click', () => connectByPseudo(false));
+    
+    socket.on('server:user:exist', () => connectByPseudo(true));
+    socket.on('server:user:connected', () => {
+        console.log(':D');
+        connectToChat();
+    });
+})
